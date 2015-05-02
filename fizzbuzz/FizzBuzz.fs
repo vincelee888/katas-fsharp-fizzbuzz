e757cbf73a333d53f4a4f2406b79c3850918ae2d
Vince Lee <vincent.lee@ao.com>
2015-05-02 18:16:36 +0100

    1 begets "1"

---
+++ fizzbuzz/FizzBuzz.fs
@@ -0,0 +1,15 @@
+﻿namespace fizzbuzz.Tests
+
+open NUnit.Framework
+open FsUnit
+
+type FizzBuzz() =
+   member x.Output input = "1"
+   
+[<TestFixture>] 
+type ``Given a FizzBuzz`` ()=
+   let fb = new FizzBuzz()
+
+   [<Test>] 
+   member test. ``1 begets "1".`` ()=
+    fb.Output 1 |> should equal "1"
