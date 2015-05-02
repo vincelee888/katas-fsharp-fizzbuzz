4cc119647aa6ff0d7ee3cc3ee0d0e25f87da936a
Vince Lee <vincent.lee@ao.com>
2015-05-02 22:42:17 +0100

    split tests into separate file

--- fizzbuzz/FizzBuzz.fs
+++ fizzbuzz/FizzBuzz.fs
@@ -1,7 +1,4 @@
-﻿namespace fizzbuzz.Tests
-
-open NUnit.Framework
-open FsUnit
+﻿namespace fizzbuzz
 
 type FizzBuzz() =
     member x.Output input = 
@@ -10,17 +7,3 @@ type FizzBuzz() =
         | input when input % 3 = 0 -> "Fizz"
         | input when input % 5 = 0 -> "Buzz"
         | _ -> input.ToString()
-   
-[<TestFixture>] 
-type ``Given a FizzBuzz`` ()=
-    let fb = new FizzBuzz()
-
-    [<TestCase(1, "1")>] 
-    [<TestCase(2, "2")>] 
-    [<TestCase(3, "Fizz")>] 
-    [<TestCase(5, "Buzz")>] 
-    [<TestCase(6, "Fizz")>] 
-    [<TestCase(10, "Buzz")>] 
-    [<TestCase(15, "FizzBuzz")>] 
-    member test. ``input begets output.`` (input, output)=
-        fb.Output input |> should equal output
---
+++ fizzbuzz/FizzBuzz_Tests.fs
@@ -0,0 +1,18 @@
+﻿namespace fizzbuzz
+
+open NUnit.Framework
+open FsUnit
+   
+[<TestFixture>] 
+type ``Given a FizzBuzz`` ()=
+    let fb = new FizzBuzz()
+
+    [<TestCase(1, "1")>] 
+    [<TestCase(2, "2")>] 
+    [<TestCase(3, "Fizz")>] 
+    [<TestCase(5, "Buzz")>] 
+    [<TestCase(6, "Fizz")>] 
+    [<TestCase(10, "Buzz")>] 
+    [<TestCase(15, "FizzBuzz")>] 
+    member test. ``input begets output.`` (input, output)=
+        fb.Output input |> should equal output
